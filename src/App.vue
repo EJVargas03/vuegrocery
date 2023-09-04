ea4de1b6915707b199b037d44f1f6f9f94ff6acc
EJVargas03 <143569160+EJVargas03@users.noreply.github.com>
2023-09-04 17:52:29 -0500

    Added some fo my own touches

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
   const deleteGrocery = id => {
     const removeIndex = groceries.value.findIndex(grocery => grocery.id === id)
     groceries.value.splice(removeIndex, 1)
-    confetti({ particleCount: 1, spread: 8000, origin: { y: 1 } })
+    confetti({ particleCount: 10, spread: 8000, origin: { y: 1 } })
   }
 </script>
 
@@ -40,6 +40,13 @@
           {{ grocery.name }}
         </li>
       </ul>
+    <h2 class="guide">How to use this program</h2>
+      <ol id="guideList">
+          <li class="guideItem">1. Type in your item</li>
+          <li class="guideItem">2. Cick the "add" button</li>
+          <li class="guideItem">3. Click the item on the list to remove it</li>
+          <li class="guideItem">4. Confetti !!!</li>
+      </ol>
   </main>
 </template>
 
@@ -47,27 +54,36 @@
   main {
       @apply mt-8 flex flex-col justify-center items-center gap-8;
     .title {
-        @apply m-2 text-6xl font-light tracking-wider text-accent;
+        @apply m-2 text-6xl font-light tracking-wider text-purplish;
+    }
+    .guide {
+        @apply flex-col pt-5 text-comment ;
     }
     form{
-      @apply flex focus-within:ring-8 focus-within:ring-accent focus-within:rounded-lg;
+      @apply flex focus-within:ring-8 focus-within:ring-purplish focus-within:rounded-lg;
       input {
         @apply bg-white text-comment p-2 w-80 text-xl rounded-l-md outline-none;
       }
       button {
-        @apply bg-accent text-background p-2 text-2xl font-bold rounded-r-md;
+        @apply bg-purplish text-background p-2 text-2xl font-bold rounded-r-md;
         &:hover {
-          @apply bg-purplish;
+          @apply bg-bluish text-purple-50;
       }
     }
   }
   ul{
     @apply flex flex-col items-center justify-center rounded-lg bg-comment;
     li {
-      @apply bg-white text-background m-2 p-2 w-96 text-center;
-      &:hover{
-        @apply bg-purplish font-bold cursor-pointer;
+        @apply bg-white text-background m-2 p-2 w-96 text-center;
+        &:hover{
+          @apply text-purplish;
+        }
       }
+    }
+    ol{
+    @apply flex-col text-comment ;
+    li {
+      @apply text-comment m-1 pl-16 w-96;
     }
   }
 }
